91d6feda6bb8cc8d717ca4280aab22def5e3a187
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-31 11:14:10 -0700

    switch

---
+++ Project4.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Project4 {
+	public static void main(String S[]) {
+		int i = 0;
+		while(i<10) {
+          System.out.println("i=" +i);
+          i++;
+		}
+	}
+}
