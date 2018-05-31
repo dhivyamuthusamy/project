82d938b10be10306c181b70d66c3d512ce7f600c
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-31 11:13:04 -0700

    if

---
+++ Project1.java
@@ -0,0 +1,15 @@
+package project;
+
+public class Project1 {
+	public static void main(String[] args) {
+		int count = 10;
+		
+		if(count < 20) {
+			System.out.println(20);
+		}
+		else{
+			System.out.println(10);
+		}
+			
+}
+}
