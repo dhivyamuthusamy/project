12923a95c24080993cf318f87de4fbebee0bfa3c
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-31 23:54:21 -0700

    hi

---
+++ Project7.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Project7 {
+	public static void main(String S[])
+	{
+		int age=18;
+	if ( age >= 65 )
+System.out.println( "Age is greater than or equal to 65" );
+else
+System.out.println( "Age is less than 65 ");
+
+	}
+
+}
