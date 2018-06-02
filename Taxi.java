22236b7c345e3f2357df632da07d5c007cb5ed3b
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-06-01 20:50:12 -0700

    super class

---
+++ Taxi.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Taxi extends Car {
+	public Taxi() {
+		System.out.println("Taxi cons");
+	}
+	public static void main(String[] args) {
+		Taxi taxi = new Taxi();
+}
+
+}
