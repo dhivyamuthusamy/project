aaff8c5e65dc3f2a66f1a6b538bb427513d246a5
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-06-03 04:38:27 -0700

    animal

---
+++ Animal.java
@@ -0,0 +1,19 @@
+package Student;
+
+public  class Animal implements Travel{
+
+	@Override
+	public void getSpeed() {
+		System.out.println("speed is 200");
+		
+	}
+
+	@Override
+	public void getColor() {
+		System.out.println("red");
+		
+	}
+
+	
+
+}
