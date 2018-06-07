1e74bed45199c0ca6310e2df83a27189c8d95cde
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-06-07 00:27:52 -0700

    string

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+package project;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		int a[]= {4,6,7,3,4};
+		for(int i=a.length-1;i>=0;i--) {
+			System.out.println(a[i]);
+			
+		}
+	}
+
+}
