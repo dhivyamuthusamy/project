cd8cd423e55ed49e6e0209f77d49e9f473955b60
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-06-12 14:09:00 +0530

    fact

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Factorial {
+	public static void main(String[] args) {
+		int i,fact=1;
+		int number=6;
+for(i=1;i<=6;i++) {
+	fact=fact*i;
+}
+		
+System.out.println("factorial"+fact);		
+	}
+
+}
